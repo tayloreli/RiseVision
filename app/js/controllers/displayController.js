60161b7bdaeedb9884cd6cb97dfd8a9704c89aae
Eli Taylor <eli.taylor@yescoelectronics.com>
2015-12-15 11:49:27 -0700

    get the correct json data

--- app/js/controllers/displayController.js
+++ app/js/controllers/displayController.js
@@ -13,8 +13,8 @@ riseApp.controller('displayController',
 				// So here we will output the result to avoid it if there is one.
 				// There is the error case so result element would not exist on the json responce
 				var resultJson = (jsonResp.result) ? jsonResp.result : jsonResp;
-				var jsonData = JSON.stringify(resultJson, undefined, 2);
-				$scope.displays = jsonData["items"];
+				//var jsonData = JSON.stringify(resultJson, undefined, 2);
+				$scope.displays = resultJson.items;
 				//output(syntaxHighlight(jsonData), type); // see output.js
 			});
 		}
