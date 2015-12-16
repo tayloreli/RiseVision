f26f6e8694cf18f1d671320d0cb0cbf920959dcf
Eli Taylor <eli.taylor@yescoelectronics.com>
2015-12-16 16:32:45 -0700

    working out using the new gapi wrapper

--- app/js/controllers/displayController.js
+++ app/js/controllers/displayController.js
@@ -1,10 +1,18 @@
 'use strict'
 
 riseApp.controller('displayController',
-	function displayController($scope, displayList){
+	function displayController($scope, cmApiService){
 		$scope.displays = displayList;
 		$scope.message = "Hello World";
 		
+		cmApiService.execute('core.display.list').then(
+			function(resp){
+				console.log(resp);
+			},
+			function(reason){
+				console.log(reason);
+			}
+		);
 		listDisplay("", "", "", "", "", "", function (jsonResp, rawResp, type) {
 			var resultJson = (jsonResp.result) ? jsonResp.result : jsonResp;
 			$scope.displays = resultJson.items;
