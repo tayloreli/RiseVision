192b73d26920c8e529cb4eac86a93f8c87d2c95e
Eli Taylor <eli.taylor@yescoelectronics.com>
2015-12-15 17:09:40 -0700

    added ui-router and resolve for the list of displays

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,46 @@
 'use strict'
 
-var riseApp = angular.module('riseApp', []);
+var riseApp = angular.module('riseApp', ['ui.router']);
+
+riseApp.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/home');
+	
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'partials/home.html'
+		})
+		
+		.state('displays', {
+			url: '/displays',
+			templateUrl: 'partials/displays.html',
+			controller: 'displayController',
+			resolve: {
+				displayList: function($stateParams, $q){
+					var parameters;
+					
+					var deferred = $q.defer();
+					
+					gapi.client.load(API_NAME, API_VER, function () {
+						var request = gapi.client.core.display.list(parameters);
+				
+						request.execute(function (jsonResp, rawResp) {
+							deferred.resolve(jsonResp, rawResp);
+						});
+						return deferred.promise;
+					}, ROOT);
+				}
+			}
+		})
+		
+		.state('schedules', {
+			url: '/schedules',
+			templateUrl: 'partials/schedules.html',
+			controller: 'scheduleController'
+		})
+		
+		.state('presentations', {
+			url: '/presentations',
+			templateUrl: 'partials/presentations.html'
+		})
+});
